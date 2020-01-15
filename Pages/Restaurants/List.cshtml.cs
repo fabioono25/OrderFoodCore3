4f6bcb5f2b23def85ffe7388c3496263cfc32ea1
fabio.tavoloni <fabio.tavoloni@LNLT-PC0YUUXY>
2020-01-15 21:29:26 +0000

    working with return of data in memory

--- Pages/Restaurants/List.cshtml.cs
+++ Pages/Restaurants/List.cshtml.cs
@@ -1,17 +1,31 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OrderFoodCore.Core;
+using OrderFoodCore3.Data;
+using System.Collections.Generic;
 
 namespace OrderFoodCore3.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public readonly IRestaurantData restaurantData;
+
+        public readonly IConfiguration config;
+
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
+
         public void OnGet()
         {
-
+            //Message = "Hello World!";
+            Message = config["Message"];
+            Restaurants = restaurantData.GetAll();
         }
     }
 }
