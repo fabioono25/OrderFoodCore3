18f5d28fcf1d0f90d4e62d8a670c4faa99ca837d
fabioono25 <fabioono25@gmail.com>
2020-01-18 17:40:39 +0000

    configure connection in startup

--- OrderFoodCore3.Data/OdeToFoodDbContext.cs
+++ OrderFoodCore3.Data/OdeToFoodDbContext.cs
@@ -5,6 +5,12 @@ namespace OrderFoodCore3.Data
 {
     public class OdeToFoodDbContext : DbContext
     {
+        public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options)
+            : base(options)
+        {
+
+        }
+
         public DbSet<Restaurant> Restaurants { get; set; }
     }
 }
