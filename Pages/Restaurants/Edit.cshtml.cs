820350f27460cc0ec5341da38d3f0448d07239b5
fabioono25 <fabioono25@gmail.com>
2020-01-18 11:44:01 +0000

    edit feature

---
+++ Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OrderFoodCore.Core;
+using OrderFoodCore3.Data;
+using System.Collections.Generic;
+
+namespace OrderFoodCore3
+{
+    public class EditModel : PageModel
+    {
+        private readonly IHtmlHelper htmlHelper;
+
+        public IRestaurantData restaurantData { get; }
+        
+        public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+        public EditModel(IRestaurantData restaurantData,
+                         IHtmlHelper htmlHelper)
+        {
+            this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
+        }
+               
+        public IActionResult OnGet(int restaurantId)
+        {
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+
+            Restaurant = restaurantData.GetById(restaurantId);
+
+            if (Restaurant == null)
+                return RedirectToPage("./NotFound");
+
+            return Page();
+        }
+    }
+}
