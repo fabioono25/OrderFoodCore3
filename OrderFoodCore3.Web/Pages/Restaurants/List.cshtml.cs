361f5f0574de02f1218704147c1f79ae4fb9c922
fabioono25 <fabioono25@gmail.com>
2020-01-23 11:48:13 +0000

    logging messages

--- OrderFoodCore3.Web/Pages/Restaurants/List.cshtml.cs
+++ OrderFoodCore3.Web/Pages/Restaurants/List.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using OrderFoodCore.Core;
 using OrderFoodCore3.Data;
 using System.Collections.Generic;
@@ -12,17 +13,20 @@ namespace OrderFoodCore3.Pages.Restaurants
         public IEnumerable<Restaurant> Restaurants { get; set; }
 
         public readonly IRestaurantData restaurantData;
-
+        private readonly ILogger<ListModel> logger;
         public readonly IConfiguration config;
 
-        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        public ListModel(IConfiguration config, IRestaurantData restaurantData, ILogger<ListModel> logger)
         {
             this.config = config;
             this.restaurantData = restaurantData;
+            this.logger = logger;
         }
 
         public void OnGet(string searchTerm)
         {
+            logger.LogError("Executing ListModel");
+
             //HttpContext.Request.Query: information about HTTP transaction
 
             //Message = "Hello World!";
